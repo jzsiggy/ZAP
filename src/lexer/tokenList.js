3b0c9b5179ff8c6e95482f9e5cba49f82b4a39e5
Joao Zsigmond <jzsiggy@gmail.com>
2020-04-09 18:56:47 -0300

    Changing tokens

--- src/lexer/tokenList.js
+++ src/lexer/tokenList.js
@@ -6,9 +6,15 @@ const tokenList = {
   "MODULO" : (input) => input == '%',
   "LPAREN" : (input) => input == '(',
   "RPAREN" : (input) => input == ')',
+  "SEMICOLON" : (input) => input ==';',
   "EQUALS" : (input) => input == '=',
-  "NUMBER" : (input) => /\d/.test(input),
-}
+  "NUMBER" : (input) => /\d/.test(input) || input == '.',
+  "LETTER" : (input) => /\w/.test(input) && !/\d/.test(input),
+  "QUOTE" : (input) => input == "'",
+  "WHITESPACE" : (input) => /\s/.test(input),
+  "SYMBOLSETTER" : (input) => input == '>',
+  "UNRECOGNIZED" : (input) => true,
+};
 
 module.exports = {
   tokenList,
