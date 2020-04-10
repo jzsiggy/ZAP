4c6582601d3464e16d21f534d07563048f6898ba
Joao Zsigmond <jzsiggy@gmail.com>
2020-04-10 01:54:52 -0300

    implementing WAYYYYY better lexer

--- src/lexer/lexer.js
+++ src/lexer/lexer.js
@@ -1,10 +1,10 @@
 const { tokenList } = require('./tokenList');
 
-class Lexer {
+class CharSeperator {
   constructor(input) {
     this.input = input;
     this.charTypes = [];
-    this.tokens = [];
+    this.getCharTypes();
   };
 
   throwErr (msg) {
@@ -24,9 +24,9 @@ class Lexer {
         if (verification(char)) {
           const charDescription = {};
           charDescription['type'] = token;
-          charDescription['symbol'] = char;
           charDescription['line'] = line;
           charDescription['col'] = col;
+          charDescription['value'] = char;
           if (charDescription.type == "UNRECOGNIZED") {
             this.throwErr(`UNRECOGNIZED SYNTAX -- ln: ${charDescription['line']} col: ${charDescription['col']}`)
           } else {
@@ -42,108 +42,152 @@ class Lexer {
         col++;
       };
     };
-    console.log(this.input);
-    console.log(this.charTypes);
-  };
-
-  tokenize() {
-    let currentToken = {};
-
-    let inVar = false;
-    let inString = false;
-    let inNum = false;
-    
-    for (let char of this.charTypes) {
-      
-      if (char.type == "QUOTE") {
-        inString = !inString;
-        if (inString) {
-          currentToken = {
-            "type" : "STRING",
-            "line" : char.line,
-            "col" : char.col,
-            "value" : "",
-          };
-        } else {
-          this.tokens.push(currentToken);
-          currentToken = {};
-        };
-        continue;
-      };
-
-      if (inString) {
-        currentToken.value = currentToken.value.concat(char.symbol);
-        continue;
-      };
-
-      if (char.type == "WHITESPACE") {
-        inVar = false;
-        inNum = false;
-        if (currentToken.type) {
-          this.tokens.push(currentToken);
-          currentToken = {};
-        };
-        continue;
-      };
-
-      if (char.type == "SYMBOLSETTER") {
-        inVar = true;
-        currentToken = {
-          "type" : "VAR",
-          "line" : char.line,
-          "col" : char.col,
-          "value" : "",
-        };
-        continue;
-      };
-
-      if (inVar && (char.type != "NUMBER" && char.type != "LETTER")) {
-        this.throwErr(`INVALID VARIABLE DEFINITION -- ln: ${char['line']} col: ${char['col']}`)
-      }
-
-      if (inVar) {
-        currentToken.value = currentToken.value.concat(char.symbol);
-        continue;
-      }
-
-      if (char.type == "NUMBER" && !inNum) {
-        inNum = true;
-        currentToken = {
-          "type" : "NUM",
-          "line" : char.line,
-          "col" : char.col,
-          "value" : char.symbol,
-        };    
-        continue;
-      };
-
-      if (char.type == "NUMBER" && inNum) {
-        currentToken.value = currentToken.value.concat(char.symbol);
-        continue;
-      };
-
-      if (inNum && char.type != "NUMBER") {
-        inNum = false;
-        this.tokens.push(currentToken);
-        currentToken = {};
-      };
-
-      if (char.type != "WHITESPACE") {
-        currentToken = {
-          "type" : char.type,
-          "line" : char.line,
-          "col" : char.col,
-          "value" : char.symbol,
-        };
-        this.tokens.push(currentToken);
-        currentToken = {};
-      };
-    };
-    console.log(this.tokens);
   };
 };
 
 
-const lexer = new Lexer(">xnciw = 20; \n>exception = 'xnciw' + 23;");
-lexer.getCharTypes();
-lexer.tokenize();
+class Lexer {
+  constructor (input) {
+    this.charSeperator = new CharSeperator(input);
+    this.charTypes = this.charSeperator.charTypes;
+    this.index = 0;
+    this.char = this.charTypes[this.index];
+    this.currentToken = {};
+    this.tokens = [];
+  }
+
+  throwErr (msg) {
+    try {
+      throw new Error(msg);
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    };
+  };
+
+  resetCurrentToken() {
+    this.currentToken = {};
+  }
+
+  next() {
+    this.index++;
+    this.char = this.charTypes[this.index];
+  };
+
+  peakNext() {
+    try {
+      return this.charTypes[this.index + 1];
+    } catch {
+      return false;
+    };
+  };
+
+  handleStr() {
+    this.currentToken = {
+      "type" : "STRING",
+      "line" : this.char.line,
+      "col" : this.char.col,
+      "value" : "",
+    };
+
+    if (this.peakNext()) {
+      this.next();
+    } else {
+      this.throwErr('EOF WHILE PARSING STRING')
+    }
+
+    while (this.char.type != "QUOTE") {
+      this.currentToken.value = this.currentToken.value.concat(this.char.value);
+      this.next();
+      if (!this.char) {
+        this.throwErr('EOF WHILE PARSING STRING')
+      };
+    };
+    this.tokens.push(this.currentToken);
+    this.resetCurrentToken();
+    this.next();
+    this.lex();
+  };
+
+  handleNum() {
+    this.currentToken = {
+      "type" : "NUMBER",
+      "line" : this.char.line,
+      "col" : this.char.col,
+      "value" : "",
+    };
+
+    while (this.char.type == "NUMBER") {
+      this.currentToken.value = this.currentToken.value.concat(this.char.value);
+      this.next();
+      if (!this.char) {
+        break;
+      };
+    };
+    this.tokens.push(this.currentToken);
+    this.resetCurrentToken();
+    this.lex();
+  };
+
+  handleId() {
+    this.currentToken = {
+      "type" : "IDENTIFIER",
+      "line" : this.char.line,
+      "col" : this.char.col,
+      "value" : "",
+    };
+
+    while (this.char.type == "NUMBER" || this.char.type == "LETTER") {
+      this.currentToken.value = this.currentToken.value.concat(this.char.value);
+      this.next();
+      if (!this.char) {
+        break;
+      };
+    };
+    this.tokens.push(this.currentToken);
+    this.resetCurrentToken();
+    this.lex();
+  };
+
+  handleOperator() {
+    if (this.char.type != "WHITESPACE") {
+      this.currentToken = this.char;
+      this.tokens.push(this.currentToken);
+      this.resetCurrentToken();
+    }
+    this.next();
+    this.lex();
+  }
+
+  lex() {
+    if (!this.char) {
+      console.log(this.tokens)
+      process.exit(1);
+    }
+    switch(this.char.type) {
+      case "QUOTE":
+        this.handleStr();
+        break;
+      case "NUMBER":
+        this.handleNum();
+        break;
+      case "LETTER":
+        this.handleId();
+        break;
+      default:
+        this.handleOperator();
+    }
+  };
+};
+
+console.time('lexing')
+
+const lexer = new Lexer("\
+>x = 20; \n\
+>r4 = 567; >ry = (x + 23) - 345;\n\
+'hey'\
+78 ;'hell0'");
+
+lexer.lex();
+
+console.timeEnd('lexing');
