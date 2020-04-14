55d671c3f158e6d7776726b201e0a8dc91b15385
Joao Zsigmond <jzsiggy@gmail.com>
2020-04-14 20:13:53 -0300

    Adding expr, print and block stmts

--- src/Interpreter/Interpreter.js
+++ src/Interpreter/Interpreter.js
@@ -1,14 +1,23 @@
+const { ErrorHandler } = require('../errorHandler/ErrorHandler');
 const { Lexer } = require('../lexer/Lexer');
 const { Parser } = require('../parser/Parser');
+
+class BlockStmt {
+  constructor(tokens) {
+    this.tokens = tokens;
+    this.statementSeparator = new StatementSeparator(tokens);
+    this.statements = this.statementSeparator.statements;
+  };
+};
 
 class PrintStmt {
   constructor(expression) {
     this.expression = expression;
     this.parser = new Parser(this.expression);
     this.value = this.parser.parse().value;
-    this.interpret();
+    // this.execute();
   };
-  interpret() {
+  execute() {
     console.log(this.value);
   };
 }
@@ -17,53 +26,130 @@ class ExprStmt {
   constructor(expression) {
     this.expression = expression;
     this.parser = new Parser(this.expression);
+    this.value = this.parser.parse().value;
   }
 }
+
+class DeclarationStmt {
+  constructor(tokens) {
+    this.value=tokens;
+  };
+};
 
 class StatementSeparator {
   constructor (tokens) {
+    this.errorHandler = new ErrorHandler();
     this.tokens = tokens;
+    this.index = 0;
+    this.currentToken = this.currentToken = this.tokens[this.index];
+    
     this.statements = [];
+    this.currentStatement = [];
+
+    this.openingBrace = 0;
+    this.closingBrace = 0;
+
+    this.checkBrace();
     this.separateStatements();
   };
 
-  separateStatements() {
-    let statement = [];
-    for (let token of this.tokens) {
-      if (token.value == ';') {
-        this.statements.push(statement);
-        statement = [];
-      } else {
-        statement.push(token);
-      };
-    }
+  resetCurrentStatement() {
+    this.currentStatement = [];
   }
-}
+
+  checkBrace() {
+    if (this.currentToken) {
+      if (this.currentToken.type == 'LBRACE') {
+        this.openingBrace++;
+      };
+      if (this.currentToken.type == 'RBRACE') {
+        this.closingBrace++;
+      };
+    };
+  };
+
+  next() {
+    this.index++;
+    this.currentToken = this.tokens[this.index];
+    this.checkBrace();
+  };
+
+  isInBlock() {
+    return this.openingBrace != this.closingBrace;
+  };
+
+  isSemicolon() {
+    return this.currentToken.type == 'SEMICOLON';
+  };
+
+  parseStatement(statement) {
+    if (statement[0].type == 'LBRACE') {
+      let stmt = new BlockStmt(statement.slice(1, -1));
+      // console.log(stmt);
+      return stmt;
+    }
+    if (statement[0].type == 'SHOW') {
+      let stmt = new PrintStmt(statement.slice(1));
+      // console.log(stmt);
+      return stmt;
+    };
+    if (statement[0].type == 'SYMBOLSETTER') {
+      let stmt = new DeclarationStmt(statement);
+      // console.log(stmt);
+      return stmt;
+    };
+    let stmt = new ExprStmt(statement);
+    // console.log(stmt);
+    return stmt;
+  };
+
+  separateStatements() {
+    while (this.currentToken) {
+      if (!this.isInBlock())
+      {
+        if (!this.isSemicolon()) {
+          this.currentStatement.push(this.currentToken);
+        } else {
+          let stmt = this.parseStatement(this.currentStatement);
+          this.statements.push(stmt);
+          this.resetCurrentStatement();
+        };
+        this.next()
+        continue;
+      } 
+      else 
+      {
+        this.currentStatement.push(this.currentToken);
+        if (!this.isInBlock()) {
+          let stmt = this.parseStatement(this.currentStatement);
+          this.statements.push(stmt);
+          this.resetCurrentStatement();
+        };
+        this.next();
+      };
+    };
+    if (this.currentStatement.length) {
+      this.errorHandler.throw(
+        'YOU MUST HAVE FORGOTTEN A SEMICOLON',
+        null,
+        null
+      );
+    };
+  };
+};
 
 class Interpreter {
   constructor(statements) {
     this.statements = statements;
   };
-
-  interpret() {
-    for (let statement of this.statements) {
-      this.parseStatement(statement);
-    };
-  };
-
-  parseStatement(statement) {
-    if (statement[0].type == 'SHOW') {
-      let stmt = new PrintStmt(statement.slice(1));
-      return stmt;
-    };
-  };
-}
+};
 
 
 
-const lexer = new Lexer("show (9+8); show 4+5; show 4; show  -- -3;");
+const lexer = new Lexer("{ 1; show 34; };1; 1+5; show 3; @a = 6;");
 const statementSeparator = new StatementSeparator(lexer.tokens);
 const { statements } = statementSeparator;
+
 const interpreter = new Interpreter(statements);
 
-interpreter.interpret()
+console.log(interpreter.statements)
