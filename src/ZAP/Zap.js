4a198eacae3bd8b0e23dc194d3321af117153d7b
Joao Zsigmond <jzsiggy@gmail.com>
2020-04-15 02:22:23 -0300

    Main class

---
+++ src/ZAP/Zap.js
@@ -0,0 +1,20 @@
+const { Interpreter } = require('../interpreter/Interpreter');
+const fs = require('fs');
+
+class Zap {
+  constructor() {
+    this.input = this.fetchInput();
+    new Interpreter(this.input);
+  }
+
+  fetchInput() {
+    try {
+      const data = fs.readFileSync('../tests/test1.zap', 'utf8');
+      return data  
+    } catch(e) {
+      console.log('Error:', e.stack);
+    }
+  };
+};
+
+new Zap();
