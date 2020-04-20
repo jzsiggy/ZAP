fcb4044975728c3b5770b6307defc83c3dab9dcb
Joao Zsigmond <jzsiggy@gmail.com>
2020-04-20 15:30:26 -0300

    Making compatible with REACT

--- jzap/src/ZAP/Zap.js
+++ jzap/src/ZAP/Zap.js
@@ -2,33 +2,40 @@ const { Interpreter } = require('../interpreter/Interpreter');
 const fs = require('fs');
 
 class Zap {
-  constructor() {
-    this.input = this.fetchInput();
+  constructor(input) {
+    this.input = input;
     this.interpreter = new Interpreter(this.input);
-  };
-
-  fetchInput() {
-    let file = process.argv[2];
-    if (!file)
-    {
-      console.log('USAGE -- [ node Zap.js <filename> ]');
-    }
-    else
-    {
-      try 
-      {
-        const data = fs.readFileSync(file, 'utf8');
-        return data  
-      } 
-      catch(e) 
-      {
-        console.log('USAGE -- [ node Zap.js <filename> ]');
-        console.log('Error:', e.stack);
-      };
-    };
   };
 };
 
-console.time('interpreting');
-new Zap();
-console.timeEnd('interpreting');
+const fetchSource = () => {
+  let file = process.argv[2];
+  if (!file)
+  {
+    console.log('USAGE -- [ node Zap.js <filename> ]');
+  }
+  else
+  {
+    try 
+    {
+      const data = fs.readFileSync(file, 'utf8');
+      return data  
+    } 
+    catch(e) 
+    {
+      console.log('USAGE -- [ node Zap.js <filename> ]');
+      console.log('Error:', e.stack);
+    };
+  };
+
+  return [];
+
+};
+
+// console.time('interpreting');
+// new Zap(fetchSource());
+// console.timeEnd('interpreting');
+
+module.exports = {
+  Zap,
+};
