e44b4be066bbc51d230c1ea5a0846a1bd33d43c3
Joao Zsigmond <jzsiggy@gmail.com>
2020-04-22 02:18:49 -0300

    Updates

---
+++ precache-manifest.f726aa8cd485fb70c1815be3022ff11e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749b47bc9f327167a8d234f9f4df235d",
+    "url": "/ZAP/index.html"
+  },
+  {
+    "revision": "03ff9762991ebb912d33",
+    "url": "/ZAP/static/css/2.9b67f534.chunk.css"
+  },
+  {
+    "revision": "e03469739fe932f07d52",
+    "url": "/ZAP/static/css/main.6b97d506.chunk.css"
+  },
+  {
+    "revision": "03ff9762991ebb912d33",
+    "url": "/ZAP/static/js/2.db75f255.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ZAP/static/js/2.db75f255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e03469739fe932f07d52",
+    "url": "/ZAP/static/js/main.3e8186d4.chunk.js"
+  },
+  {
+    "revision": "df6d4b50b5ad9f45b6be",
+    "url": "/ZAP/static/js/runtime-main.034d0317.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ZAP/web/precache-manifest.310973eee90d9c02143952c85fcd6946.js"
+  "/ZAP/precache-manifest.f726aa8cd485fb70c1815be3022ff11e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ZAP/web/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ZAP/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
